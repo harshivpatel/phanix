85e06f29c78e9690da030ea992a83807581bb4a8
harshivpatel <hp200378@gmail.com>
2026-03-04 12:56:20 +0000

    feat: resolve merge conflicts and update configuration for bootloader and kernel setup

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! { loop {} }
-
 
 static PHANIX: &[u8] = b"phanix";
 
@@ -12,11 +9,18 @@ static PHANIX: &[u8] = b"phanix";
 pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
 
-    for(i, &byte) in PHANIX.iter().enumerate() {
+    for (i, &byte) in PHANIX.iter().enumerate() {
         unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            let line_offset: isize = 0; 
+            
+            let char_offset = (i as isize * 2) + line_offset;
+            let color_offset = (i as isize * 2 + 1) + line_offset;
+
+            *vga_buffer.offset(char_offset) = byte;
+            *vga_buffer.offset(color_offset) = 0x0b;
         }
     }
     loop {}
 }
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! { loop {} }
