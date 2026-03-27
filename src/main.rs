9421ff9df873ec9dd81e7bd4cecc72f7ffd8a702
harshivpatel <hp200378@gmail.com>
2026-03-27 22:13:40 +0000

    feat: implement custom print and println macros for VGA output

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@
 
     #[unsafe(no_mangle)]
     pub extern "C" fn _start() -> ! {
-        use core::fmt::Write;
-        vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-        write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-
+        println!("Hello World{}", "!");
+        panic!("Testing the custom panic handler!");
         loop {}
     }    
 
     #[panic_handler]
-    fn panic(_info: &PanicInfo) -> ! {
+    fn panic(info: &PanicInfo) -> ! {
+        println!("{}", info);
         loop {}
     }
+
