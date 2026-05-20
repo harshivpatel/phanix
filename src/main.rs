d16e04a0974976421ca6a22c5ba51ac046b7f181
harshivpatel <hp200378@gmail.com>
2026-05-20 13:03:23 +0100

    feat: add QEMU exit codes and implement exit_qemu function for test completion

--- src/main.rs
+++ src/main.rs
@@ -39,4 +39,21 @@
         for test in tests {
             test();
         }
+        exit_qemu(QeumuExitCode::Success);
+    }
+
+    #[derive(Debug, Copy, Clone, PartialEq, Eq)]
+    #[repr(u32)]
+
+    pub enum QeumuExitCode {
+        Success = 0x10,
+        Failed = 0x11,
+    }
+
+    pub fn exit_qemu(exit_code: QeumuExitCode) {
+        use x86_64::instructions::port::Port;
+        unsafe {
+            let mut port = Port::new(0xf4);
+            port.write(exit_code as u32);
+        }
     }
