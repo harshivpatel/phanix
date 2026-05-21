43811baa90f40ba9a4388a176c1ae98214b95d99
harshivpatel <hp200378@gmail.com>
2026-05-21 11:57:42 +0100

    feat: add UART 16550 support with serial printing macros and exit handling

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@
 
     mod vga_buffer;
 
+    mod serial;
+
     use core::panic::PanicInfo;
 
     static PHANIX: &[u8] = b"phanix";
@@ -21,36 +23,72 @@
 
     #[test_case]
     fn trivial_assertions() {
-        print!("trivial assertions... ");
         assert_eq!(1, 1);
-        println!("[ok")
     }
 
+    // Any type that implements this trait must provide a "run" function
+    pub trait Testable {
+        fn run(&self) -> ();
+    }
+    // A blanket implementation that automatically applies the Testable trait 
+    // to ALL functions in the project that accept 0 argument.
+    impl<T> Testable for T
+    where
+        T: Fn(),
+        {
+            fn run(&self) {
+                // Extract and print the full compiler symbol path name of the function over serial
+                serial_print!("{}...\t", core::any::type_name::<T>());
+
+                // Execute the actual function body itself
+                self();
+
+                // Print the success status
+                serial_println!("[ok]");
+            }
+        }
+
+    #[cfg(not(test))]
     #[panic_handler]
     fn panic(info: &PanicInfo) -> ! {
         println!("{}", info);
         loop {}
     }
 
+    // Panic handler in test mode
+    #[cfg(test)]
+    #[panic_handler]
+    fn panic(info: &PanicInfo) -> ! {
+        serial_println!("[failed]\n");
+        serial_println!("Error: {}", info);
+        exit_qemu(QemuExitCode::Failed);
+        loop {}
+    }
 
     #[cfg(test)]
-    pub fn test_runner(tests: &[&dyn Fn()]) {
-        println!("Running {} tests ", tests.len());
+    pub fn test_runner(tests: &[&dyn Testable]) {
+        serial_println!("Running {} tests ", tests.len());
         for test in tests {
-            test();
+            test.run();
         }
-        exit_qemu(QeumuExitCode::Success);
+        exit_qemu(QemuExitCode::Success);
+    }
+    #[test_case]
+    fn trivial_assertions() {
+        serial_print!("trivial assertions... ");
+        assert_eq!(1, 1);
+        serial_println!("[ok]");
     }
 
     #[derive(Debug, Copy, Clone, PartialEq, Eq)]
     #[repr(u32)]
 
-    pub enum QeumuExitCode {
+    pub enum QemuExitCode {
         Success = 0x10,
         Failed = 0x11,
     }
 
-    pub fn exit_qemu(exit_code: QeumuExitCode) {
+    pub fn exit_qemu(exit_code: QemuExitCode) {
         use x86_64::instructions::port::Port;
         unsafe {
             let mut port = Port::new(0xf4);
---
+++ src/serial.rs
@@ -0,0 +1,41 @@
+use uart_16550::{Config, Uart16550Tty, backend::PioBackend};
+use spin::Mutex;
+use lazy_static::lazy_static;
+
+// Normal static variables in Rust must be initialized.
+// Here initializing a hardware port requires runtime instructions, so we wrap this in a lazy_static Macro
+lazy_static! {
+    pub static ref SERIAL1: Mutex<Uart16550Tty<PioBackend>> = Mutex::new(unsafe {
+        // Creates an instance of Uart16550Tty driver stuct and initializes UART hardware interface
+        Uart16550Tty::new_port(0x3F8, Config::default())
+        .expect("failed to initialize UART")
+    });
+}
+
+// Hidden tella cargo doc to not include this function in documentation
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
+    use core::fmt::Write;
+    SERIAL1.lock().write_fmt(args).expect("Printing to serial failed")
+}
+
+// Prints to the host though the serial interface
+// Makes macro availabe globally
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*));
+    };
+}
+
+// Prints to the host through the serial interface, appending a newline
+#[macro_export]
+macro_rules! serial_println {
+    // If nothing is passed, just move down one line
+    () =>($crate::serial_print!("\n"));
+    // If one single string is passed, append \n onto the end at compile time
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    // If a string is passed with a variables like {}, it appends newline and passes passes extra tokens
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
+        concat!($fmt, "\n"), $($arg)*));
+}
